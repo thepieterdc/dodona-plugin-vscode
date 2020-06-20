a2b83268507a23ef17ea307d16d28c56eb83b051
Pieter De Clercq <pieterdeclercq@outlook.com>
2020-06-20 21:10:23 +0200

    Implement submitting to hardcoded exercise with hardcoded token

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,35 @@
 import * as vscode from 'vscode';
+import * as bent from 'bent';
+
+const post = bent('https://dodona.ugent.be', 'POST', 'json')
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log("test");
+    const disp = vscode.commands.registerCommand('extension.submit', async () => {
+        // Get the current editor.
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            // Get the code.
+            const code = editor.document.getText();
 
-    const disp = vscode.commands.registerCommand('extension.helloWorld', () => {
-        vscode.window.showInformationMessage('Hello!');
+            // Submit the code to Dodona.
+            const headers = {
+                'Authorization': 'token'
+            }
+
+            const body = {
+                "submission": {
+                    "code": code,
+                    "course_id": null,
+                    "series_id": null,
+                    "exercise_id": 1545120484
+                }
+            }
+
+            const response = await post('/submissions.json', body, headers);
+
+            // Send a notification message.
+            vscode.window.showInformationMessage('Solution submitted!');
+        }
     });
 
     context.subscriptions.push(disp);
