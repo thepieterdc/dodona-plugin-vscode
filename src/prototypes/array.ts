538efc1f9a0583b35fbc8aad0617546883acf85f
Stijn De Clercq <declercq.stijn@outlook.com>
2020-10-07 23:32:11 +0200

    Clean up notification check

---
+++ src/prototypes/array.ts
@@ -0,0 +1,23 @@
+export {};
+
+declare global {
+    interface Array<T> {
+        /**
+         * Finds max element in an Array.
+         *
+         * @example [1, 2].max() == 2
+         * @return max element
+         */
+        max(): number;
+    }
+}
+
+Array.prototype.max = function () {
+    if (this.length === 0) {
+        return 0;
+    }
+
+    return this.reduce((acc, value) => {
+        return Math.max(acc, value);
+    }, 0);
+};
