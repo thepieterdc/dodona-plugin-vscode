546ba4a84499ecbaff4495f9950234c87bcf076c
Stijn De Clercq <declercq.stijn@outlook.com>
2020-12-18 00:19:35 +0100

    Allow tree view to be filtered by academic year and course id (Fixes #74)

--- src/treeView/dataProvider.ts
+++ src/treeView/dataProvider.ts
@@ -8,7 +8,11 @@ import {
 import { AbstractTreeItem } from "./items/abstractTreeItem";
 import execute from "../api/client";
 import { CourseTreeItem } from "./items/courseTreeItem";
-import { getSortOption } from "../configuration";
+import {
+    getSortOption,
+    getYearFilter,
+    getCourseFilter,
+} from "../configuration";
 import { Course } from "../api/resources/course";
 
 // TODO add an icon for course & series to make them easier to separate in the view
@@ -36,8 +40,8 @@ export default class RootDataProvider
         // Get the courses the user is subscribed to.
         return (
             execute(dodona => dodona.courses.subscribed)
-                // Sort them alphabetically.
-                .then(cs => this.sortCourses(cs))
+                // Sort courses & apply filters
+                .then(cs => this.sortCourses(this.filterCourses(cs)))
                 // Convert them to tree items.
                 .then(cs => cs.map(c => new CourseTreeItem(c)))
         );
@@ -88,5 +92,40 @@ export default class RootDataProvider
                     : 0,
             );
         }
+    }
+
+    filterCourses(courses: Course[]): Course[] {
+        // Remove spaces that were added accidentally by students ("a, b" -> "a,b")
+        const yearFilter = getYearFilter().replace(/\s/g, "");
+        const courseFilter = getCourseFilter().replace(/\s/g, "");
+
+        // Apply filters if they were supplied
+        if (yearFilter) {
+            // Filter out double/trailing comma's & empty entries/invalid entries
+            const year_filters = yearFilter.split(",").filter(
+                // Check for valid years
+                y => !isNaN(+y) && y.length === 4 && parseInt(y) >= 2016,
+            );
+
+            // Remove courses from other academic years
+            courses = courses.filter(c =>
+                year_filters.includes(c.year.substring(0, 4)),
+            );
+        }
+
+        if (courseFilter) {
+            // Filter out double/trailing comma's & empty entries/invalid entries
+            const course_filters = courseFilter
+                .split(",")
+                // Check for valid course id's
+                .filter(c => !isNaN(+c) && c.length > 0 && parseInt(c) >= 0);
+
+            // Remove courses with id's that aren't in the list
+            courses = courses.filter(c =>
+                course_filters.includes(c.id.toString()),
+            );
+        }
+
+        return courses;
     }
 }
