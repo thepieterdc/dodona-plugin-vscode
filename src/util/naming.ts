c8c37af3878066a15590fca2e3e61922cddc2121
Pieter De Clercq <pieterdeclercq@outlook.com>
2021-04-17 00:41:23 +0200

    Generate cleansed filenames

---
+++ src/util/naming.ts
@@ -0,0 +1,25 @@
+import { Exercise } from "../api/resources/activities";
+
+// Non-alphanumeric characters.
+const NON_ALPHA = /[^a-zA-Z0-9-_. ]/gi;
+// Whitespace.
+const WHITESPACE = /\s+/g;
+
+/**
+ * Generates a filename for the given exercise.
+ *
+ * @param exercise the exercise
+ */
+export function generateFilename(exercise: Exercise): string {
+    // TODO Support generation of Java class names.
+
+    // Get the basename.
+    const basename = exercise.name
+        // Strip out all non-alphanumeric characters.
+        .replace(NON_ALPHA, "")
+        // Replace all whitespace by underscores.
+        .replace(WHITESPACE, "_");
+
+    // Append the extension of the programming language.
+    return `${basename}.${exercise.programming_language?.extension || "txt"}`;
+}
