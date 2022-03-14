f9229d535b7907f95ab2d9874d4cf6a53f6069d2
BrechtW <56451049+BTWS2@users.noreply.github.com>
2022-03-14 23:20:16 +0100

    better UX for RequestError

--- src/constants/actions.ts
+++ src/constants/actions.ts
@@ -1,3 +1,4 @@
 export const OPEN_NOTIFICATIONS_ACTION = "Open Notifications";
 export const OPEN_SETTINGS_ACTION = "Open Settings";
 export const VIEW_INSTRUCTIONS_ACTION = "View Instructions";
+export const FIX_ERROR = "Fix Error";
