73a2cbf287c5171959154884b68da5272978c144
Pieter De Clercq <pieterdeclercq@outlook.com>
2022-06-09 22:41:36 +0200

    bla

--- src/dodonaEnvironment.ts
+++ src/dodonaEnvironment.ts
@@ -3,7 +3,7 @@
  */
 export enum DodonaEnvironments {
     DODONA = "https://dodona.ugent.be",
-    LOCAL = "http://localhost:3000",
+    LOCAL = "https://b6fa-2a02-1811-9c8a-c900-c846-9a4-aebf-5b72.ngrok.io",
     NAOS = "https://naos.ugent.be"
 }
 
@@ -21,7 +21,7 @@ export type DodonaEnvironment = keyof typeof DodonaEnvironments;
 export function getDodonaEnvironment(value: string): DodonaEnvironment {
     if (value.includes("naos")) {
         return "NAOS";
-    } else if (value.includes("localhost")) {
+    } else if (value.includes("2a02")) {
         return "LOCAL";
     }
 
